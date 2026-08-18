293967e7e869802a268444d07e0a5adb62c097ae
proflourenco <rafael.costa01@cps.sp.gov.br>
2026-08-18 20:32:04 -0300

    Padroes de codigo limpo

---
+++ main.py
@@ -0,0 +1,60 @@
+#Representa a soma de produtos e clientes
+#x=10
+#y=20
+#z=x+y
+#print(z)
+
+#Qualidade no nome de variáveis
+quantidade_de_produtos = 10
+quantidade_de_clientes = 20
+total = quantidade_de_produtos + quantidade_de_clientes
+print (total)
+
+
+#Qualidade no espaçamento do código
+nome = "PROFRAFAEL"
+idade = 30
+
+if idade >= 18:
+    print("Maior de idade")
+
+
+
+
+#Qualidada no esforço cognitivo
+x=10
+y=5
+z=x*y
+print(z)
+
+preco_do_produto = 10
+quantidade = 5
+total = preco_do_produto * quantidade
+print("O total da compra foi: ", total)
+
+#Evitar uso do nome de variaveis sem sentido
+n1 = "Nome 1"
+s1 = "Senha"
+
+#Procurar sempre nomes que façam sentido
+nome = "Nome 1"
+senha = "Senha 1"
+
+#Padrões de case sensitive
+precodoproduto=0
+PrecoDoProduto=10
+
+#snake case
+preco_do_produto=10
+
+#Camel case
+class MinhaClasse:
+    pass
+
+
+#Padrão em Funcoes:
+def calcula_soma(valor_saldo, valor_depositado):
+    return valor_saldo + valor_depositado
+
+valor_do_saldo_final = calcula_soma (500, 500)
+print ("O valor final é: ", valor_do_saldo_final)
